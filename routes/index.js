6570dae1c23bf41ead0396a8ef3cdd1015549172
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Air.local>
2021-09-27 16:02:14 -0400

    basic file structure and nav bar addded

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* GET home page. */
+router.get('/home', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* GET About page. */
+router.get('/about', function (req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* GET Product page. */
+router.get('/products', function (req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+/* GET Services page. */
+router.get('/services', function (req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+/* GET About Us page. */
+router.get('/about', function (req, res, next) {
+  res.render('index', { title: 'About Us' });
+});
+/* GET Contact us page. */
+router.get('/contact', function (req, res, next) {
+  res.render('index', { title: 'Contact Us' });
+});
+
+module.exports = router;
