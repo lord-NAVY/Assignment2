70ff81095d5681b8278c850f03bc51b318461a1a
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Air.local>
2021-10-02 00:12:42 -0400

    testing commit

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET home page. */
 router.get('/home', function (req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET About page. */
 router.get('/about', function (req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Us', page: 'about' });
 });
 /* GET Product page. */
 router.get('/products', function (req, res, next) {
-  res.render('index', { title: 'Products' });
+  res.render('index', { title: 'Our Products', page: 'products' });
 });
 /* GET Services page. */
 router.get('/services', function (req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Our Services', page: 'services' });
 });
 /* GET About Us page. */
 router.get('/about', function (req, res, next) {
-  res.render('index', { title: 'About Us' });
+  res.render('index', { title: 'About Us', page: 'about' });
 });
 /* GET Contact us page. */
 router.get('/contact', function (req, res, next) {
-  res.render('index', { title: 'Contact Us' });
+  res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 
 module.exports = router;
